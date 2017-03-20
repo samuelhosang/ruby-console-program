1ba5719d3b10889a0978c8f7c0dca407d3389795
Sam Hosang <shosang@customerdirect.com>
2017-03-20 10:51:27 -0500

    Testing?

--- start.rb
+++ start.rb
@@ -3,6 +3,7 @@ require 'console_program'
 ConsoleProgram.initialize
 
 =begin
+ testing git.
   Since this file is part of the project, but not the gem, I figured it would be a good place to put random TO DOs.
 -- To Do List --
   TODO (Required) Build a way of holding loaded Modules and reloading them. -shosang
